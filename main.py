3dd43d581bee3352070779fda23794035b1c4ac8
nerdsee <jan@stoevesand.org>
2021-06-20 14:50:44 +0200

    Works 1.0

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
+import gym
+import rlutil
+import numpy as np
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+env = gym.make("FrozenLake8x8-v0")
+# env = gym.make("FrozenLake-v0")
 
+stateValues, stateActions = rlutil.getStateValues(env)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+num_steps = 100
+success = 0
+attempts = 100
 
+for tries in range(attempts):
+    total = 0
+    state = env.reset()
+    next_step = stateActions[state]
+    for sx in range(num_steps):
+        new_state, reward, done, info = env.step(next_step)
+        # env.render()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        next_step = stateActions[new_state]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        if done:
+            total = reward
+            # print("Reward:", reward)
+            break
+    if (total>0):
+        success = success + 1
+    # print("Attempt:", tries, "- reward:", total)
+print("Success:", success, "/", attempts)
